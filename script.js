61c1288f6daa7c6e0899b702d4a6118128554ede
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-02-03 02:57:41 +0100

    add joke to speed

--- script.js
+++ script.js
@@ -110,10 +110,16 @@ const VoiceRSS = {
   },
 };
 
-function test() {
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
   VoiceRSS.speech({
     key: '40dc4c8648434306832fd3125b44d656',
-    src: 'Hello, world!',
+    src: joke,
     hl: 'en-us',
     v: 'Linda',
     r: 0,
@@ -122,4 +128,32 @@ function test() {
     ssml: false,
   });
 }
-test();
+
+// Get joke from joke api
+
+async function getJokes() {
+  let joke = '';
+  const apiUrl =
+    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Text=to-Speech
+    tellMe(joke);
+    // Disavle Button
+    toggleButton();
+  } catch (error) {
+    // Catch Reeors Headers
+    console.log('whoops', error);
+  }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
